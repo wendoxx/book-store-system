632ecd6f67329c00c3ff0e4b76eca72af683b095
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-10-30 21:10:04 -0300

    first commit

---
+++ src/main/java/org/example/bookstoresystem/BookStoreSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.bookstoresystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bookstoresystem/BookStoreSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookstoresystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
