a7e458d95a40efd09236f5ce0e326844a4feb449
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-10-31 00:19:03 -0300

    feat: add BookModel

---
+++ src/main/java/org/example/bookstoresystem/model/BookModel.java
@@ -0,0 +1,27 @@
+package org.example.bookstoresystem.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "tb_books")
+@Data
+public class BookModel {
+
+    @Id
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private double price;
+
+    //TODO: add attribute "author" and manyToMany relationship
+}
