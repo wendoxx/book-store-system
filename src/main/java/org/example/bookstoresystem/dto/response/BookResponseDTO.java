d2180b93fa259158f96e43f32673921c8b644742
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-11-04 17:29:23 -0300

    feat: add constructor in BookResponseDTO with BookModelParameter

--- src/main/java/org/example/bookstoresystem/dto/response/BookResponseDTO.java
+++ src/main/java/org/example/bookstoresystem/dto/response/BookResponseDTO.java
@@ -1,15 +1,26 @@
 package org.example.bookstoresystem.dto.response;
 
 import lombok.Data;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.example.bookstoresystem.model.BookModel;
 
 import java.time.LocalDate;
 
 @Data
-public class BookModel {
+public class BookResponseDTO {
     private Long id;
     private String title;
     private String description;
     private String isbn;
     private LocalDate releaseDate;
     private double price;
+
+    public BookResponseDTO(BookModel bookModel){
+        this.id = bookModel.getId();
+        this.title = bookModel.getTitle();
+        this.description = bookModel.getDescription();
+        this.isbn = bookModel.getIsbn();
+        this.releaseDate = bookModel.getReleaseDate();
+        this.price = bookModel.getPrice();
+    }
 }
