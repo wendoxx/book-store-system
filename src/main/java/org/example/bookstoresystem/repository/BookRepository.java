3f3e6e90b8372b0c3fcf5f8b2597929c6003a07e
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-11-05 23:51:26 -0300

    feat: remove method to find books by author

--- src/main/java/org/example/bookstoresystem/repository/BookRepository.java
+++ src/main/java/org/example/bookstoresystem/repository/BookRepository.java
@@ -1,13 +1,9 @@
 package org.example.bookstoresystem.repository;
 
-import org.example.bookstoresystem.model.AuthorModel;
 import org.example.bookstoresystem.model.BookModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface BookRepository extends JpaRepository<BookModel, Long> {
-    Optional<AuthorModel> findAllByAuthor(String author);
 }
