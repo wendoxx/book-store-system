66d978dbda3e60332e4f0c1664af1de7874dcbea
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-11-06 22:22:13 -0300

    feat: add contructor in AuthorResponseDTO to convert type in service

--- src/main/java/org/example/bookstoresystem/dto/response/AuthorResponseDTO.java
+++ src/main/java/org/example/bookstoresystem/dto/response/AuthorResponseDTO.java
@@ -1,13 +1,21 @@
 package org.example.bookstoresystem.dto.response;
 
 import lombok.Data;
+import org.example.bookstoresystem.model.AuthorModel;
 import org.example.bookstoresystem.model.BookModel;
 
 import java.util.List;
+import java.util.Set;
 
 @Data
 public class AuthorResponseDTO {
     private Long id;
     private String name;
-    private List<BookModel> books;
+    private Set<BookModel> books;
+
+    public AuthorResponseDTO(AuthorModel authorModel){
+        this.id = authorModel.getId();
+        this.name = authorModel.getName();
+        this.books = authorModel.getBooks();
+    }
 }
