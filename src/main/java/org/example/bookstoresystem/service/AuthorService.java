3aaaea2f25285e295a68796197515705e66703a7
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-11-06 22:33:50 -0300

    feat: add methods to get an author by id, name and a method to delete author

--- src/main/java/org/example/bookstoresystem/service/AuthorService.java
+++ src/main/java/org/example/bookstoresystem/service/AuthorService.java
@@ -1,10 +1,12 @@
 package org.example.bookstoresystem.service;
 
 import org.example.bookstoresystem.dto.request.AuthorRequestDTO;
+import org.example.bookstoresystem.dto.response.AuthorResponseDTO;
 import org.example.bookstoresystem.model.AuthorModel;
 import org.example.bookstoresystem.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class AuthorService {
@@ -19,6 +21,22 @@ public class AuthorService {
         author.setName(authorRequestDTO.getName());
 
         return authorRepository.save(author);
+    }
+
+    public AuthorResponseDTO getAuthorById(Long id){
+        return authorRepository.findById(id)
+                .map(AuthorResponseDTO::new)
+                .orElseThrow(() -> new RuntimeException("Author not found."));
+    }
+
+    public AuthorResponseDTO getAuthorByName(String name){
+        return authorRepository.findByName(name)
+                .map(AuthorResponseDTO::new)
+                .orElseThrow(() -> new RuntimeException("Author not found"));
+    }
+
+    public void deleteAuthorById(Long id){
+        authorRepository.deleteById(id);
     }
 
 }
