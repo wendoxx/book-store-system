553577cf9f1297ec39bd2b5ada657e00c8a935be
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-11-07 18:08:31 -0300

    feat: add new security filter to authenticate token

---
+++ src/main/java/org/example/bookstoresystem/infra/SecurityFilter.java
@@ -0,0 +1,47 @@
+package org.example.bookstoresystem.infra;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.bookstoresystem.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        var token = recoverToken(request);
+
+        if (token != null){
+            var username = tokenService.validateToken(token);
+            UserDetails user = userRepository.findByUsername(username);
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+    }
+
+    public String recoverToken(HttpServletRequest httpServletRequest){
+        var authHeader = httpServletRequest.getHeader("Authorization");
+        if (authHeader == null){
+            return null;
+        }
+        return authHeader.replace("Bearer ", "");
+    }
+}
