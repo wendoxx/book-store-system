4d3ae8a3567cbfb3857aba42511d069bbf9ae1ed
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-11-06 23:30:25 -0300

    feat: add method to find user by username in UserRepository

---
+++ src/main/java/org/example/bookstoresystem/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package org.example.bookstoresystem.repository;
+
+import org.example.bookstoresystem.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+    UserModel findByUsername(String username);
+}
