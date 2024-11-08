a76a2979179feec7a6dcaef9edcb89505c9c9edb
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-11-07 22:58:08 -0300

    feat: add logs in AuthorService

--- src/main/java/org/example/bookstoresystem/service/AuthorService.java
+++ src/main/java/org/example/bookstoresystem/service/AuthorService.java
@@ -1,6 +1,8 @@
 package org.example.bookstoresystem.service;
 
 import jakarta.transaction.Transactional;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.example.bookstoresystem.dto.request.AuthorRequestDTO;
 import org.example.bookstoresystem.dto.response.AuthorResponseDTO;
 import org.example.bookstoresystem.model.AuthorModel;
@@ -17,13 +19,17 @@ public class AuthorService {
     @Autowired
     AuthorRepository authorRepository;
 
+    private static final Logger LOGGER = LogManager.getLogger();
+
     @Transactional
     public AuthorModel saveAndUpdateAuthor(AuthorRequestDTO authorRequestDTO){
         AuthorModel author;
 
         if (authorRequestDTO.getId() != null && authorRepository.existsById(authorRequestDTO.getId())){
+            LOGGER.info("Updating author...");
             author = authorRepository.findById(authorRequestDTO.getId()).get();
         }else {
+            LOGGER.info("Saving author...");
             author = new AuthorModel();
         }
 
@@ -34,22 +40,32 @@ public class AuthorService {
     }
 
     public AuthorResponseDTO getAuthorById(Long id){
+        LOGGER.info("Getting author...");
         return authorRepository.findById(id)
                 .map(AuthorResponseDTO::new)
-                .orElseThrow(() -> new RuntimeException("Author not found."));
+                .orElseThrow(() -> {
+                    LOGGER.error("Author not found.");
+                    return new RuntimeException("Author not found.");
+                });
     }
 
     public AuthorResponseDTO getAuthorByName(String name){
+        LOGGER.info("Getting author");
         return authorRepository.findByName(name)
                 .map(AuthorResponseDTO::new)
-                .orElseThrow(() -> new RuntimeException("Author not found"));
+                .orElseThrow(() -> {
+                    LOGGER.error("Author not found.");
+                    return new RuntimeException("Author not found");
+                });
     }
 
     public List<AuthorResponseDTO> getAllAuthors(){
+        LOGGER.info("Getting all authors...");
         return authorRepository.findAll().stream().map(AuthorResponseDTO::new).toList();
     }
 
     public void deleteAuthorById(Long id){
+        LOGGER.info("Deleting author...");
         authorRepository.deleteById(id);
     }
 
